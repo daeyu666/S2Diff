f9f9acb49c13bc8b5246a9c917d07190e2f3115f
daeyu666 <384114681@qq.com>
2026-09-01 11:15:45 +0800

    Export predictor V2

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,9 @@
 """Model package for S2Diff."""
 
 from .predictor import CleanHSIPredictor
+from .predictor_v2 import SpectralSpatialCleanHSIPredictor
 
-__all__ = ["CleanHSIPredictor"]
+__all__ = [
+    "CleanHSIPredictor",
+    "SpectralSpatialCleanHSIPredictor",
+]

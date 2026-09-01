1f02384cd3ab9fac75ef6dd11a83b60d178bfad8
daeyu666 <384114681@qq.com>
2026-09-01 16:14:42 +0800

    Export Innovation 2 ablation predictor

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,15 @@
 from .predictor import CleanHSIPredictor
 from .predictor_v2 import SpectralSpatialCleanHSIPredictor
 from .predictor_v3 import MSIHighFrequencyGuidedPredictor
+from .predictor_v3_ablation import (
+    MSIAblationGuidedPredictor,
+    VALID_MSI_ABLATIONS,
+)
 
 __all__ = [
     "CleanHSIPredictor",
     "SpectralSpatialCleanHSIPredictor",
     "MSIHighFrequencyGuidedPredictor",
+    "MSIAblationGuidedPredictor",
+    "VALID_MSI_ABLATIONS",
 ]
